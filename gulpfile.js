fef91dd8e0949a05b58fe3826e32890fe15df105
Andrey Pogorelov <namtydapro@gmail.com>
2019-11-15 18:08:39 +0300

    new version

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-
-gulp.task('concat', function() {
-  return gulp.src('./styles/**/*.css')
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('./dist/'));
-});
